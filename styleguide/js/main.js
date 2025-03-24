bd82621d1fed68f810eff400739250153186ac80
Brayden Barclay <164232325+braydenbarclay@users.noreply.github.com>
2025-03-24 12:34:12 -0400

    Added Animation

---
+++ styleguide/js/main.js
@@ -0,0 +1,20 @@
+
+  $(document).ready(function(){
+      $('.slider').slick({
+          infinite: true,       // Infinite loop
+          slidesToShow: 1,      // Show 1 slide at a time
+          slidesToScroll: 1,    // Scroll 1 slide at a time
+          autoplay: true,       // Enable auto-scroll
+          autoplaySpeed: 2000,  // Slide change every 2s
+          arrows: false,        // Hide prev/next buttons (optional)
+          dots: true,           // Enable navigation dots
+          responsive: [
+              {
+                  breakpoint: 768, // Mobile screens
+                  settings: {
+                      slidesToShow: 1
+                  }
+              }
+          ]
+      });
+  });
